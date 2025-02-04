c0e3bfbc2291986a3cb359da1e1db2a4733c719e
Sadiq0909 <8604sadiq@gmail.com>
2025-02-04 12:27:52 +0530

    Enhance signup functionality with user existence check and password hashing; update dependencies and toast notifications

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,18 +1,24 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export async function connect() {
     try {
-        mongoose.connect(process.env.MONGO_URI!)
-        const connection= mongoose.connection;
+        mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
 
-        connection.on('connected',()=>{
-            console.log("Connected to database successfully");
+        connection.on('connected', () => {
+            console.log('MongoDB connected successfully');
         })
-        connection.on('error',(error)=>{
-            console.log("Error connecting to database", error);
+
+        connection.on('error', (err) => {
+            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
+            process.exit();
         })
 
     } catch (error) {
-        console.log("Error connecting to database Before", error);
+        console.log('Something goes wrong!');
+        console.log(error);
+        
     }
+
+
 }
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,35 +1,34 @@
 import mongoose from "mongoose";
-import { unique } from "next/dist/build/utils";
 
 const userSchema = new mongoose.Schema({
-    username :{
-        type : String,
-        required : [true , "Provide a username"],
-        unique : true
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true,
     },
-    email :{
-        type : String,
-        required : [true , "Provide a email"],
-        unique : true
+    email: {
+        type: String,
+        required: [true, "Please provide a email"],
+        unique: true,
     },
-    password :{
-        type : String,
-        required : [true , "Provide a password"],
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
     },
-    isVerified:{
-        type :Boolean ,
-        default : false
+    isVerfied: {
+        type: Boolean,
+        default: false,
     },
-    isAdmin :{
-        type : Boolean,
-        default : false
+    isAdmin: {
+        type: Boolean,
+        default: false,
     },
-    forgotPasswordToken :String,
-    forgotPasswordTokenExpiry : Date,
-    verifyToken : String,
-    verifyTokenExpiry : Date,
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
 })
 
-const User = mongoose.models.users || mongoose.model('users', userSchema);
+const User = mongoose.models.users || mongoose.model("users", userSchema);
 
-export default User ;
+export default User;
