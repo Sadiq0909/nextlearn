d335dac09de457af30cbc438d70a75298f2b27d3
Sadiq0909 <8604sadiq@gmail.com>
2025-02-04 15:55:52 +0530

    Implement logout functionality with API route and user feedback; add logout button to profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,35 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import toast from "react-hot-toast"
+;import {useRouter} from "next/navigation";
+
 export default function ProfilePage() {
+
+    const router = useRouter()
+
+    const logout=async()=>{
+        try {
+            await axios.get("/api/users/logout")
+            toast.success("Logout Successful")
+            router.push("/login")
+        } catch (error :any) {
+            console.log(error.message);
+            toast.error(error.message)
+        }
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>profile page</p>
+            <hr />
+            <button
+            onClick = {logout} 
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 hover:bg-lime-400 hover:text-black">
+                Logout
+            </button>
         </div>
     )
 }
