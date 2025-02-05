0160b34e1ce33ae2fc3c0ecf9ab021a6ec24d971
Sadiq0909 <8604sadiq@gmail.com>
2025-02-05 19:03:09 +0530

    Add user details retrieval functionality to profile page; implement button for fetching user data

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import axios from "axios";
+import React from "react";
 import Link from "next/link";
 import toast from "react-hot-toast"
 ;import {useRouter} from "next/navigation";
@@ -7,6 +8,7 @@ import toast from "react-hot-toast"
 export default function ProfilePage() {
 
     const router = useRouter()
+    const [data ,setData] = React.useState("Nothing")
 
     const logout=async()=>{
         try {
@@ -18,6 +20,11 @@ export default function ProfilePage() {
             toast.error(error.message)
         }
     }
+    const getUserDetails = async()=>{
+        const res = await axios.get("/api/users/me") ;
+        console.log(res.data);
+        setData(res.data.data._id) ; 
+    }
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -25,11 +32,18 @@ export default function ProfilePage() {
             <hr />
             <p>profile page</p>
             <hr />
+            <h2 className="bg-orange-400 p-3 m-3">{data === "Nothing" ? "Nothing to show" : <Link href={`/profile/${data}`}>{data}</Link>}</h2>
             <button
             onClick = {logout} 
             className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 hover:bg-lime-400 hover:text-black">
                 Logout
             </button>
+            <button
+            onClick = {getUserDetails} 
+            className="p-2 border bg-neutral-600 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 hover:bg-lime-400 hover:text-black">
+                Get User Details
+            </button>
+            
         </div>
     )
 }
