c26ce0c1c7d0210d41e5b8ba1bd94d4592136821
Sadiq0909 <8604sadiq@gmail.com>
2025-02-06 12:15:09 +0530

    Add email verification page and fix user verification logic

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -17,7 +17,7 @@ export async function POST(request:NextRequest){
         }
 
         console.log(user);
-        user.isVerified = true ;
+        user.isVerfied = true ;
         user.verifyToken = undefined ; 
         user.verifyTokenExpiry = undefined ; 
         await user.save() ; 
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server'
  
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname ;
-  const isPublic = path === "/login" || path === "/signup"
+  const isPublic = path === "/login" || path === "/signup" || path === "/verifyemail"
 
   const token = request.cookies.get("token")?.value || ""
   if(isPublic && token){
@@ -20,5 +20,6 @@ export const config = {
     "/profile",
     "/login",
     "/signup",
+    "/verifyemail"
   ]
 }
